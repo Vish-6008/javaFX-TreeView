3ea53c38b2e80e6bd6d3b6a045141b4455efb22d
DELL <DELL@DESKTOP-8S0EJGG>
2024-06-20 11:29:29 +0530

    License of the code has been updated.

--- TreeViewProject/src/application/TreeMainController.java
+++ TreeViewProject/src/application/TreeMainController.java
@@ -6,15 +6,11 @@
  *---------------------------------------------------------------------------
  *   Purpose:  JAVA-FX project
  *   Project:  TreeMainController
- *   Platform: Cloud Service Provider Independent
- *   Compiler: JavaSE-11
- *   IDE:	   Eclipse IDE for Enterprise Java and Web Developers (includes Incubating components)
-
- *			   Version: 2022-02 (4.0.0)
- *             Build id: 202202
- ********************************************************************************************/
-
-
+ *   Platform: JavaSE-22.0.1
+ *   IDE:  	   Eclipse IDE for Enterprise Java and Web Developers (includes Incubating components)
+ * 	 Version: 2024-03 (4.31.0)
+ *   Build id: 20240307-1437 
+ *********************************************************************************************/
 
 package application;
 
@@ -31,36 +27,31 @@ import javafx.scene.image.ImageView;
 public class TreeMainController implements Initializable {
 	@FXML
 	TreeView<String> treeView;
-	
-	
-	//Adding the image on the list icon
-	Image icon=new Image(
-			getClass().getResourceAsStream("/img/pngwing.png"));
+
+	// Adding the image on the list icon
+	Image icon = new Image(getClass().getResourceAsStream("/img/pngwing.png"));
 
 	@Override
 	public void initialize(URL Location, ResourceBundle arg1) {
-		TreeItem<String> root=new TreeItem<>("Root",new ImageView(icon));
+		TreeItem<String> root = new TreeItem<>("Root", new ImageView(icon));
 		// TODO Auto-generated method stub
-		
-		TreeItem<String> nodeA=new TreeItem<>("node A");
-		TreeItem<String> nodeB=new TreeItem<>("node B");
-		TreeItem<String> nodeC=new TreeItem<>("node C");
-		root.getChildren().addAll(nodeA,nodeB,nodeC);		//easy way to call
 
-		
-		
-		//to Add & get SubNodes for above main nodes
-		TreeItem<String> nodeA1=new TreeItem<>("node A1",new ImageView(icon));
-		TreeItem<String> nodeB1=new TreeItem<>("node B1",new ImageView(icon));
-		TreeItem<String> nodeC1=new TreeItem<>("node C1",new ImageView(icon));
-		nodeA.getChildren().addAll(nodeA1,nodeB1,nodeC1);
+		TreeItem<String> nodeA = new TreeItem<>("node A");
+		TreeItem<String> nodeB = new TreeItem<>("node B");
+		TreeItem<String> nodeC = new TreeItem<>("node C");
+		root.getChildren().addAll(nodeA, nodeB, nodeC); // easy way to call
+
+		// to Add & get SubNodes for above main nodes
+		TreeItem<String> nodeA1 = new TreeItem<>("node A1", new ImageView(icon));
+		TreeItem<String> nodeB1 = new TreeItem<>("node B1", new ImageView(icon));
+		TreeItem<String> nodeC1 = new TreeItem<>("node C1", new ImageView(icon));
+		nodeA.getChildren().addAll(nodeA1, nodeB1, nodeC1);
 		root.getChildren().add(nodeA);
 		root.getChildren().add(nodeB);
 		root.getChildren().add(nodeC);
-		
-		
+
 		treeView.setRoot(root);
-		
+
 	}
 
 }
